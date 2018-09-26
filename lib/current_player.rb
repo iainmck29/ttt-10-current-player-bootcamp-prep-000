395ef21f921982e97f9a8e7dbb768178686a3215
Iain McKenzie <iainmck29@hotmail.com>
2018-09-26 09:19:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,15 @@ board.each do |spaces|
   if spaces == "X" || spaces == "O"
     counter += 1
   end
-
 end
 return counter
+end
+
+def current_player(board)
+  if turn_count(board) == 0
+    return "X"
+  elsif turn_count(board) % 2 == 0
+    return "X"
+  else return "O"
+  end
 end
